4a7122941c9c690b5415abf97f4f701d26095a86
JoeDobos <138618822+JoeDobos@users.noreply.github.com>
2024-01-01 13:51:08 -0600

    Add files via upload

---
+++ Poisson Simulation.py
@@ -0,0 +1,38 @@
+import random
+import matplotlib.pyplot
+
+from random import expovariate
+from matplotlib.pyplot import *
+
+Avg_IAT = 2.0
+Avg_ST = 1.0 #avg service time
+Sim_Time = 100 #total simulation time
+N = 0
+clock = 0
+
+X = [] #times of events
+Y = [] #values of N
+
+while clock <= Sim_Time:
+    IAT = expovariate(1 / Avg_IAT)
+    ST = expovariate(1 / Avg_ST)
+    if IAT <= ST:
+        N += 1
+        clock = clock + IAT
+        X.append(clock)
+        Y.append(N)
+    else:
+        if N > 0:
+            N -= 1
+            clock = clock + ST
+            X.append(clock)
+            Y.append(N)
+
+
+step(X, Y, Linewidth = 1.2, color = 'black')
+xlabel('Time' , size = 16)
+ylabel('N' , size = 16)
+xlim(0, 101)
+show()
+savefig('sim_birth_death_process.pdf' , format='pdf' , bbox_inches='tight')
+
